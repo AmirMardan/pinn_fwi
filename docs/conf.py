6a33b9f4ca8091c0ee977bba820b7bd36a9b97b3
Amir Mardan <46511946+AmirMardan@users.noreply.github.com>
2024-03-28 01:56:27 -0400

    fix the name of repo in conf

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# don't forget to change html_context['github_repo'] in bottom of this file
 project = 'PINN-FWI'
 copyright = '2023, Amir Mardan'
 author = 'Amir Mardan'
@@ -132,6 +133,6 @@ html_context['downloads'].append( ('epub', '/' +REPO_NAME+ '/' +current_language
  
 html_context['display_github'] = True
 html_context['github_user'] = 'amirmardan'
-html_context['github_repo'] = 'piann_fwi'
+html_context['github_repo'] = 'pinn_fwi'
 html_context['github_version'] = 'main/docs/'
  
